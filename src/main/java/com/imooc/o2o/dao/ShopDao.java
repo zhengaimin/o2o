dfe4de0e2b555e6425ee09d7d918d82b11f0f6c6
zhengaimin <1210846754@qq.com>
2018-09-10 18:02:47 +0800

    o2o

---
+++ src/main/java/com/imooc/o2o/dao/ShopDao.java
@@ -0,0 +1,18 @@
+package com.imooc.o2o.dao;
+
+import com.imooc.o2o.entity.Shop;
+
+/**
+ * @author 郑爱民
+ * 日期: 2018-09-10
+ * 时间: 16:58
+ */
+public interface ShopDao {
+
+    /**
+     * 新增店铺
+     * @param shop
+     * @return
+     */
+    int insertShop(Shop shop) ;
+}
