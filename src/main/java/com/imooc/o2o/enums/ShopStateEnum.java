af02f55e3199e5df511cfa3b43c757236d90a894
zhengaimin <1210846754@qq.com>
2018-09-12 19:04:57 +0800

    o2o

---
+++ src/main/java/com/imooc/o2o/enums/ShopStateEnum.java
@@ -0,0 +1,48 @@
+package com.imooc.o2o.enums;
+
+/**
+ * @author 郑爱民
+ * 日期: 2018-09-11
+ * 时间: 10:51
+ */
+public enum ShopStateEnum {
+    CHECK(0,"审核中"),
+    OFFLINE(-1,"非法店铺"),
+    SUCCESS(1,"操作成功"),
+    PASS(2,"通过认证"),
+    INNER_ERROR(-1001,"内部系统错误"),
+    NULL_SHOPID(-1002,"shopId为空"),
+    ;
+
+    private int state ;
+
+    private String stateInfo ;
+
+    ShopStateEnum(int state, String stateInfo) {
+        this.state = state;
+        this.stateInfo = stateInfo;
+    }
+
+    public int getState() {
+        return state;
+    }
+
+    public String getStateInfo() {
+        return stateInfo;
+    }
+
+    /**
+     * 依据传入的state返回相应的enum值
+     * @param state
+     * @return
+     */
+    public static ShopStateEnum stateOf(int state) {
+        for (ShopStateEnum stateEnum : values()) {
+            if (stateEnum.getState() == state) {
+                return stateEnum ;
+            }
+        }
+        return null ;
+    }
+
+}
